46f487c833ca07396e5621d592a19612882e4a00
burcuicen <burcuicen2000@gmail.com>
2021-10-11 20:13:06 +0300

    adding animation to dices

--- dice/src/Die.js
+++ dice/src/Die.js
@@ -1,9 +1,14 @@
-import React,{Component} from "react";
+import React, { Component } from 'react'
 
-
-class Die extends Component{
-    render(){
-        return <i className={` Die fas fa-dice-${this.props.face}`}></i>
-    }
+class Die extends Component {
+  render() {
+    return (
+      <i
+        className={` Die fas fa-dice-${this.props.face} ${
+          this.props.rolling ? 'shaking' : ''
+        }`}
+      ></i>
+    )
+  }
 }
-export default Die;
+export default Die
--- dice/src/RollDice.js
+++ dice/src/RollDice.js
@@ -1,43 +1,46 @@
-import React, { Component } from 'react';
-import Die from './Die';
-import "./RollDice.css";
+import React, { Component } from 'react'
+import Die from './Die'
+import './RollDice.css'
 
 class RollDice extends Component {
   static defaultProps = {
     //an array of possible outcomes
     sides: ['one', 'two', 'three', 'four', 'five', 'six'],
-  };
+  }
   constructor(props) {
-    super(props);
-    this.state = { die1: 'one', die2: 'one' }; //default state of dices
-    this.roll=this.roll.bind(this);
+    super(props)
+    this.state = { die1: 'one', die2: 'one', isRolling: false } //default state of dices
+    this.roll = this.roll.bind(this)
   }
   roll() {
     //pick 2 new rolls
-    const newDie1 =
-      this.props.sides[
-        Math.floor(Math.random() * this.props.sides.length)
-      ]; //randomly selects a number(0-6) and selects the index in array
-    const newDie2 =
-      this.props.sides[
-        Math.floor(Math.random() * this.props.sides.length)
-      ]; //randomly selects a number(0-6) and selects the index in array
+    const newDie1 = this.props.sides[
+      Math.floor(Math.random() * this.props.sides.length)
+    ] //randomly selects a number(0-6) and selects the index in array
+    const newDie2 = this.props.sides[
+      Math.floor(Math.random() * this.props.sides.length)
+    ] //randomly selects a number(0-6) and selects the index in array
 
     //set the state with new rolls
-    this.setState({die1:newDie1,die2:newDie2})
+    this.setState({ die1: newDie1, die2: newDie2, isRolling: true })
+    //wait one second and isRolling should turn to false
+    setTimeout(() => {
+      this.setState({ isRolling: false })
+    }, 1000)
   }
   render() {
     return (
       <div className="rollDice">
-          <div className="container">
-          <Die face={this.state.die1}></Die>
-        <Die face={this.state.die2}></Die>
+        <div className="container">
+          <Die face={this.state.die1} rolling={this.state.isRolling}></Die>
+          <Die face={this.state.die2} rolling={this.state.isRolling}></Die>
+        </div>
 
-          </div>
-       
-        <button onClick={this.roll}>Roll The Dice</button>
+        <button onClick={this.roll} disabled={this.state.isRolling}>
+          {this.state.isRolling ? 'Rolling...' : 'Roll The Dice'}
+        </button>
       </div>
-    );
+    )
   }
 }
-export default RollDice;
+export default RollDice
